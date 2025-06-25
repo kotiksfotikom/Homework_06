2294cee02be27e8b919971ec64b06804e8290120
DimaSuh <kotiksfotikom@gmail.com>
2025-06-25 21:21:30 +0300

    megachill

--- src/main/java/com/cicd/Main.java
+++ src/main/java/com/cicd/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.printf("Hello and welcome!\n");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println(Methods.idkMethod(5,i));
+            System.out.println(Methods.idkMethod(5, i));
         }
     }
 }
